bfd30150fdfd68660b4158e8d5984e733c56a94d
kevin-maciel <kevin.maciel@moxit.net>
2022-10-06 14:15:37 -0300

    Add print reporte

---
+++ ProjectXaf.Module/Controllers/PrintSemiterminadosController.cs
@@ -0,0 +1,77 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Actions;
+using DevExpress.ExpressApp.Editors;
+using DevExpress.ExpressApp.Layout;
+using DevExpress.ExpressApp.Model.NodeGenerators;
+using DevExpress.ExpressApp.ReportsV2;
+using DevExpress.ExpressApp.SystemModule;
+using DevExpress.ExpressApp.Templates;
+using DevExpress.ExpressApp.Utils;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Persistent.Validation;
+using DevExpress.XtraPrinting;
+using DevExpress.XtraReports.UI;
+using ProjectXaf.Module.BusinessObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectXaf.Module.Controllers
+{
+    // For more typical usage scenarios, be sure to check out https://documentation.devexpress.com/eXpressAppFramework/clsDevExpressExpressAppViewControllertopic.aspx.
+    public partial class PrintSemiterminadosController : ViewController
+    {
+        // Use CodeRush to create Controllers and Actions with a few keystrokes.
+        // https://docs.devexpress.com/CodeRushForRoslyn/403133/
+        public PrintSemiterminadosController()
+        {
+            InitializeComponent();
+            // Target required Views (via the TargetXXX properties) and create their Actions.
+            TargetObjectType = typeof(Semiterminados);
+            SimpleAction printAction = new SimpleAction(this, "Imprimir semi", PredefinedCategory.Reports);
+            printAction.ImageName = "Action_Printing_Print";
+            printAction.Execute += delegate (object sender, SimpleActionExecuteEventArgs e) {
+                IObjectSpace objectSpace = ReportDataProvider.ReportObjectSpaceProvider.CreateObjectSpace(typeof(ReportDataV2));
+                IReportDataV2 reportData = objectSpace.FirstOrDefault<ReportDataV2>(data => data.DisplayName == "Reporte Semiterminados");
+                if (reportData == null)
+                {
+                    throw new UserFriendlyException("Cannot find the 'Contacts Report' report.");
+                }
+                else
+                {
+                    PrintReport(reportData);
+                }
+            };
+        }
+        protected void PrintReport(IReportDataV2 reportData)
+        {
+            ReportsModuleV2 reportsModule = ReportsModuleV2.FindReportsModule(Application.Modules);
+            if (reportsModule != null && reportsModule.ReportsDataSourceHelper != null)
+            {
+                XtraReport report = ReportDataProvider.ReportsStorage.LoadReport(reportData);
+                reportsModule.ReportsDataSourceHelper.SetupBeforePrint(report);
+                report.CreateDocument();
+                PrintToolBase tool = new PrintToolBase(report.PrintingSystem);
+                tool.Print();
+            }
+        }
+        protected override void OnActivated()
+        {
+            base.OnActivated();
+            // Perform various tasks depending on the target View.
+        }
+        protected override void OnViewControlsCreated()
+        {
+            base.OnViewControlsCreated();
+            // Access and customize the target View control.
+        }
+        protected override void OnDeactivated()
+        {
+            // Unsubscribe from previously subscribed events and release other references and resources.
+            base.OnDeactivated();
+        }
+    }
+}
